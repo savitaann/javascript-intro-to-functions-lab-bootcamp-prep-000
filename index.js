90defc5644eb8acd3155c8fedff2e266f6654246
savita madray <savitamadray01@gmail.com>
2017-09-28 18:11:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,3 +18,8 @@ function sayHiToGrandma(string) {
   if(string === string.toLowerCase())
   return "I can't hear you!";
 }
+
+function sayHiToGrandma(string) {
+  if (string === string.toUpperCase())
+  return "YES INDEED!"
+}
